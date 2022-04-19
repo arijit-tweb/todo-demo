1042a68a9570134293bc390c8499f95e9cb187cc
Arijit <arijitmaiti085@gmail.com>
2022-04-19 18:35:46 +0530

    added authentication

--- src/components/auth/Login.js
+++ src/components/auth/Login.js
@@ -7,6 +7,7 @@ import { useDocumnetTitle } from '../../helpers/setDocumentTitle';
 
 const Login = () => {
 
+  // custom hook for page name 
   useDocumnetTitle("login");
 
   const location = useLocation();
