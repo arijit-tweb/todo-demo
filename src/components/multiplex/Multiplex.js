e436ceecd3194df3f165852e3faadd5e31092fe5
Arijit <arijitmaiti085@gmail.com>
2022-04-21 18:58:50 +0530

    apr 21

--- src/components/multiplex/Multiplex.js
+++ src/components/multiplex/Multiplex.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
 import { useDispatch } from 'react-redux';
 import { addMultiplex } from '../../redux/reducers/multiplex';
 
@@ -12,49 +12,55 @@ const Multiplex = () => {
 
     const [err, setErr] = useState('');
 
-    const inputChange = (e)=>{
+    const inputChange = (e) => {
         setMultiplexValue({
             ...multiplexValue,
             [e.target.name]: e.target.value
         })
     };
 
-    const addMultiplexadded = ()=>{
-        const {name, screen, seatType} = multiplexValue
-       if(name === '' || screen === '' || seatType === ''){
-           setErr("field can't be Blank");
-           return
-       }
-       setErr('');
-       dispatch(addMultiplex(multiplexValue))
+    const addMultiplexadded = () => {
+        const { name, screen, seatType } = multiplexValue
+        if (name === '' || screen === '' || seatType === '') {
+            setErr("field can't be Blank");
+            return
+        }
+        setErr('');
+        dispatch(addMultiplex(multiplexValue))
 
-    }
+    };
+
+    useEffect(()=>{
+
+    },[])
+
+    const multiplex = localStorage.getItem('multiplex');
 
     return (
         <>
             <h4 className="text-center mb-4">Add Multiplex</h4>
 
-                <p className="text-center my-2">
-                    {
-                        err && err
-                    }
-                </p>
+            <p className="text-center my-2">
+                {
+                    err && err
+                }
+            </p>
 
             <div className="w-full max-w-xs m-auto">
                 <input className="shadow appearance-none border rounded mb-3 w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" id="username" type="text" placeholder="Multiplex Name"
-                name="name"
-                value={multiplexValue.name}
-                onChange={inputChange}
+                    name="name"
+                    value={multiplexValue.name}
+                    onChange={inputChange}
                 />
                 <input className="shadow appearance-none border mb-3 rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" id="username" type="text" placeholder="Screen"
-                name="screen"
-                value={multiplexValue.screen}
-                onChange={inputChange}
+                    name="screen"
+                    value={multiplexValue.screen}
+                    onChange={inputChange}
                 />
                 <select className="form-select appearance-none block w-full px-3 py-1.5 text-base font-normal text-gray-700 bg-white bg-clip-padding bg-no-repeat border border-solid border-gray-300 rounded transition ease-in-out m-0 focus:text-gray-700 focus:bg-white focus:border-blue-600 focus:outline-none"
-                name="seatType"
-                value={multiplexValue.seatType}
-                onChange={inputChange}
+                    name="seatType"
+                    value={multiplexValue.seatType}
+                    onChange={inputChange}
                 >
                     <option value=''>Select Seat Type</option>
                     <option value="club">Club</option>
@@ -63,11 +69,19 @@ const Multiplex = () => {
                 </select>
 
                 <button className="my-3 bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline" type="button" onClick={addMultiplexadded}>
-                        Add
+                    Add
                 </button>
+            </div>
+
+            <div className="my-5">
+               
             </div>
         </>
     )
+}
+
+const PerMultiplex = () => {
+
 }
 
 export default Multiplex
